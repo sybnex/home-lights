99416a3d7caa8f5bc0ad76e5f18ff67b628c2eab
sybnex <sybnex@gmail.com>
2019-10-22 22:49:40 +0200

    correcting

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f ~/.installed ]; then
+if [ ! -f /tmp/.installed ]; then
   # install files
   sudo apt update
   sudo apt install python3-pip git -y
@@ -9,12 +9,8 @@ if [ ! -f ~/.installed ]; then
   sudo pip3 install --upgrade pip
   sudo pip3 install --upgrade -r requirements.txt
 
-  touch ~/.installed
+  touch /tmp/.installed
 fi
 
 # add crontabfile
 crontab crontab.file
-
-# ensure bot is running
-test -n "$(pgrep -f bot.py)" || sudo python3 /home/pi/home-lights/bot.py
-
