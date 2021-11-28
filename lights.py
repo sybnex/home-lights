36a767e4c1ac8b04b21a57c8bb56395eee34864f
Daniel S <sybnex@gmail.com>
2021-11-28 18:32:34 +0100

    change timing

--- lights.py
+++ lights.py
@@ -163,7 +163,7 @@ if aqua2power in devices:
   # Aqua2 stuff
   if checkRange(23,  4):                          setSwitch(aqua2power, "-f1", "Heater2 off")
   else:                                           setSwitch(aqua2power, "-o1", "Heater2 on")
-  if checkRange( 8, 12) or checkRange(16, 20):    setSwitch(aqua2power, "-o2", "Aqua2 lights on")
+  if checkRange( 9, 12) or checkRange(17, 21):    setSwitch(aqua2power, "-o2", "Aqua2 lights on")
   else:                                           setSwitch(aqua2power, "-f2", "Aqua2 lights off")
   # if checkRange( 7, 10) or checkRange(19, 21):    setSwitch(aqua2power, "-o3", "Mini light on")
   # else:                                           setSwitch(aqua2power, "-f3", "Mini light off")
