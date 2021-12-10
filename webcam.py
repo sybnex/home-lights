2ad42e86a57803fd4fcd8c7f3b3f91d10542b403
Daniel S <sybnex@gmail.com>
2021-12-10 18:20:45 +0100

    redesign

--- webcam.py
+++ webcam.py
@@ -28,9 +28,10 @@ weekdays = (1,2,3,4,5)
 script_path = os.path.abspath(".")
 template_path = f"{script_path}/template.jinja2"
 
-# check
-if not os.path.exists(org_image):
-  sys.exit(1)
+def check_path:
+    if not os.path.exists(org_image):
+        return False
+    return True
 
 # note data
 def getData(ident):
@@ -42,35 +43,43 @@ def getData(ident):
     print(f"WARN: Could not get {ident} data: {err}")
     return None, None
 
-sunset, sunrise = getData("weather")
-if sunrise and sunset:
-    sun_am = datetime.datetime.fromtimestamp(sunrise) - datetime.timedelta(hours = 0.5)
-    sun_pm = datetime.datetime.fromtimestamp(sunset) + datetime.timedelta(hours = 0.5)
-    if now < sun_am:
-        print(f"{now}: too early: {sun_am}")
+def sunshine():
+    sunset, sunrise = getData("weather")
+    if sunrise and sunset:
+        sun_am = datetime.datetime.fromtimestamp(sunrise) - datetime.timedelta(hours = 0.5)
+        sun_pm = datetime.datetime.fromtimestamp(sunset) + datetime.timedelta(hours = 0.5)
+        if now < sun_am:
+            print(f"{now}: too early: {sun_am}")
+            return False
+        if now > sun_pm:
+            print(f"{now}: too late: {sun_pm}")
+            return False
+    return True
+
+def make_picture(location=org_image):
+    camera = PiCamera()
+    camera.resolution = (1920, 1080)
+    camera.framerate = 10
+    camera.zoom = (0.3,0.1,0.8,0.8)
+    camera.start_preview()
+    time.sleep(15)
+    camera.capture(location)
+    camera.stop_preview()
+
+if __name__ == "__main__":
+
+    if not check_path():
         sys.exit(1)
-    if now > sun_pm:
-        print(f"{now}: too late: {sun_pm}")
-        sys.exit(1)
 
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.framerate = 10
-camera.zoom = (0.3,0.1,0.8,0.8)
-camera.start_preview()
+    if sunshine:
+        make_picture()
+        if weekday in weekdays:
+            subprocess.run(f"sudo cp {org_image} {new_image}", shell=True)
 
-time.sleep(15)
-
-camera.capture(org_image)
-camera.stop_preview()
-
-if weekday in weekdays:
-    subprocess.run(f"sudo cp {org_image} {new_image}", shell=True)
-
-if jinja:
-    stamp = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
-    jinja2_template_string = open(template_path, 'r').read()
-    template = Template(jinja2_template_string)
-    html_template_string = template.render(timestamp=stamp)
-    with open("/home/pi/nas/index.html", "w") as result_file:
-        result_file.write(html_template_string)
+        if jinja:
+            stamp = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
+            jinja2_template_string = open(template_path, 'r').read()
+            template = Template(jinja2_template_string)
+            html_template_string = template.render(timestamp=stamp)
+            with open("/home/pi/nas/index.html", "w") as result_file:
+                result_file.write(html_template_string)
